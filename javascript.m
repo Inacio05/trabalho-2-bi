8206003c1cc030503ccbb0de703e335a9601ba86
Inacio05 <145071609+Inacio05@users.noreply.github.com>
2023-11-19 22:21:16 -0300

    subir arquivos do meu site

---
+++ javascript.m
@@ -0,0 +1,9 @@
+const questions = [
+    {
+        question: "Qual é o maior mamífero terrestre?",
+        options: ["Elefante", "Girafa", "Hipopótamo", "Rinoceronte"],
+        correctAnswer: "Elefante"
+    },
+    // Adicione mais perguntas conforme necessário
+];
+
